2f8fe0b6573b18e0d1109634d51850584b52046a
Ashish12X <ashishkumarkeshri8822@gmail.com>
2026-02-25 21:29:29 +0530

    Add reset button functionality to game

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ let main=document.querySelector("main");
 
 let turnX=true;
 
+//Win Array
 const winArr=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
 boxes.forEach((box)=>{
@@ -65,4 +66,5 @@ const checkWinner=()=>{
 };
 
 newBtn.addEventListener("click",resetGame);
+
 resetBtn.addEventListener("click",resetGame);
